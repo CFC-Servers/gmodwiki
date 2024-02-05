a888aece147aa9d37de083291f95015556a99555
Brandon Sturgeon <brandon@brandonsturgeon.com>
2024-02-04 16:29:12 -0800

    wip

--- build/index.ts
+++ build/index.ts
@@ -1,3 +1,5 @@
+import * as cheerio from "cheerio"
+import { promises as fs } from "fs"
 import ApiInterface from "./api_interface.js"
 
 const baseURL = "https://wiki.facepunch.com"
@@ -7,8 +9,53 @@ async function getIndex() {
     return await api.get("/gmod")
 }
 
-async function init() {
-    const index = await getIndex()
+function processFooters($: any) {
+    const footers = $("div.footer")
+
+    // Contains the section for views / last updated
+    const firstFooter = footers.first()
+    firstFooter.html("{footer}")
+
+    // Contains the section for the links
+    const secondFooter = footers.last()
+    secondFooter.remove()
 }
 
-init();
+function processContent($: any) {
+    $("div.body-tabs").remove()
+    const content = $("div[id='pagecontent']")
+    content.html("<slot />")
+}
+
+function extractSidebar($: any): string {
+    const sidebar = $("div[id='sidebar']")
+    const contents = sidebar.html()
+    sidebar.html("<Sidebar></Sidebar>")
+
+    return contents
+}
+
+function replaceVariables($: any) {
+    $("title").html("{title}")
+    $("meta[name='og:title']").attr("content", "{title}")
+    $("meta[name='og:description']").attr("content", "{description}")
+}
+
+async function build() {
+    const index = await getIndex()
+    const $ = cheerio.load(index)
+
+    processFooters($)
+    processContent($)
+    replaceVariables($)
+
+    let sidebar = extractSidebar($)
+    sidebar = sidebar.trim()
+    await fs.writeFile("src/components/Sidebar.astro", sidebar)
+
+    let layout = $.html()
+    layout = layout.replace("<sidebar></sidebar>", "<Sidebar />")
+    await fs.appendFile("src/layouts/Layout.astro", layout)
+}
+
+build();
