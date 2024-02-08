8fff2b014137f4d12b09d48feaa6dcf36e1c6f2f
Brandon Sturgeon <brandon@brandonsturgeon.com>
2024-02-07 17:29:41 -0800

    Use localstorage for darkmode instead

--- build/darkmode.js
+++ build/darkmode.js
@@ -94,24 +94,11 @@ function removeGlobalStyle() {
 }
 
 function setDarkMode(value) {
-    const days = 365;
-    let expires = "";
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
-
-    document.cookie = `darkMode=${value}${expires}; path=/; SameSite=Lax${location.protocol === 'https:' ? '; Secure' : ''}`;
+    localStorage.setItem("darkMode", value);
 }
 
 function getDarkMode() {
-    const nameEQ = 'darkMode=';
-    const ca = document.cookie.split(';');
-    for(let i=0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null; // Return null if the darkMode cookie has not been set
+    return localStorage.getItem("darkMode");
 }
 
 let darkModeEnabled = getDarkMode() === "true";
