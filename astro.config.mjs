2ed2e1db88e3c712a1d54e723f2959aeee30fd09
Redox <git@redox.to>
2025-11-30 01:14:17 +0100

    Add minification

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 
 import node from "@astrojs/node";
 import cloudflare from "@astrojs/cloudflare";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import playformCompress from "@playform/compress";
 
 const buildEnv = process.env.BUILD_ENV;
 
@@ -22,8 +24,70 @@ if (buildEnv === "production") {
   };
 }
 
+const DEFAULT_OPTIONS = {
+  includePublic: true,
+  svg: {
+    multipass: true,
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            cleanupNumericValues: true,
+            cleanupIds: {
+              minify: true,
+              remove: true,
+            },
+            convertPathData: true,
+          },
+        },
+      },
+      'sortAttrs',
+      {
+        name: 'addAttributesToSVGElement',
+        params: {
+          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+        },
+      },
+    ],
+  },
+  png: {
+    quality: 90,
+  },
+  jpeg: {
+    quality: 90,
+  },
+  jpg: {
+    quality: 90,
+  },
+  tiff: {
+    quality: 90,
+  },
+  gif: {},
+  webp: {
+    lossless: false,
+    quality: 90,
+  },
+  avif: {
+    lossless: false,
+  },
+  cache: false,
+  cacheLocation: undefined,
+};
+
 export default defineConfig({
   build: buildConfig,
   output: "server",
   adapter: adapter,
+  compressHTML: true,
+
+  vite : {
+    plugins: [
+      ViteImageOptimizer(DEFAULT_OPTIONS)
+    ]
+  },
+
+  integrations: [playformCompress({
+    Image: false
+  })]
 });
